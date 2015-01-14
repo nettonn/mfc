06db98403fe67006d33f84519eed0aeac0b04b34
Dmitry Lebedev <dev.nettonn@gmail.com>
2015-01-14 18:26:54 +0300

    delete tests

--- config/console.php
+++ config/console.php
@@ -1,12 +1,10 @@
 <?php
 
-Yii::setAlias('@tests', dirname(__DIR__) . '/tests');
-
 $params = require(__DIR__ . '/params.php');
 $db = require(__DIR__ . '/db.php');
 
 return [
-    'id' => 'basic-console',
+    'id' => 'cms-console',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log', 'gii'],
     'controllerNamespace' => 'app\modules\main\commands',
--- config/web.php
+++ config/web.php
@@ -3,7 +3,7 @@
 $params = require(__DIR__ . '/params.php');
 
 $config = [
-    'id' => 'CMS',
+    'id' => 'cms',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'language'=>'ru_RU',
